59f359db340527ef756faf035bd4cf38299d7023
Evgeniy Nikitin <shotalime@mail.ru>
2020-06-04 21:13:02 +0300

    added long polling, fixes some bugs

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,30 @@
 module Main where
 
-import Control.Monad
-import Control.Monad.IO.Class
+import           Control.Monad
+import           Control.Monad.IO.Class
 import           Control.Concurrent
-import Data.Aeson
-import qualified Data.Text as T
-import GHC.Generics
-import Network.HTTP.Req
-import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy as L
+import           Data.Aeson
+import qualified Data.Text                     as T
+import           GHC.Generics
+import           Network.HTTP.Req
+import qualified Data.ByteString.Char8         as B
+import qualified Data.ByteString.Lazy          as L
 import           Data.Maybe                     ( fromJust )
-import qualified Text.URI as URI
-import Telegram.Bot.API.Types
-import BotConfig
-import System.Timeout
+import qualified Text.URI                      as URI
+import           Telegram.Bot.API.Types
+import           BotConfig
+-- import           System.Timeout
 
 
-data MyData = MyData
-  { size  :: Int
-  , color :: T.Text
+data MessageToSend =  MessageToSend
+  { chat_id  :: Int
+  , text :: T.Text
   } deriving (Show, Generic)
 
-instance ToJSON MyData
-instance FromJSON MyData
+
+
+instance ToJSON MessageToSend
+instance FromJSON MessageToSend
 
 api :: T.Text
 api = "api.telegram.org"
@@ -30,12 +32,17 @@ api = "api.telegram.org"
 botToken :: T.Text
 botToken = mconcat ["bot", token]
 
+getOffset :: Maybe Update -> Integer
+getOffset Nothing = 0
+getOffset (Just update) = updateId update
 
-getLastUpdate :: Updates -> Update
-getLastUpdate  = last . result
+getLastUpdate ::  Updates -> Maybe Update
+getLastUpdate updates = case result updates of
+  [] -> Nothing
+  _ -> Just $ last $ result updates
 
 getMessage :: Update -> Maybe Message
-getMessage  =  updateMessage
+getMessage = updateMessage
 
 getChat :: Maybe Message -> Chat
 getChat (Just m) = messageChat m
@@ -44,68 +51,50 @@ getChatId :: Update -> Int
 getChatId = chatId . getChat . getMessage
 
 getText :: Update -> Maybe T.Text
-getText u =  getMessage u >>=  messageText
+getText u = getMessage u >>= messageText
 
-getUpdateId :: Update -> Integer
-getUpdateId = updateId
+getBodyResponse ::  LbsResponse -> HttpResponseBody LbsResponse
+getBodyResponse  = responseBody
 
-isNewUpdate :: Update -> Update -> Bool
-isNewUpdate u1 u2 = getUpdateId u1 /= getUpdateId u2
+makeUpdateRequest :: Integer -> Req LbsResponse
+makeUpdateRequest offset = req POST
+                        (https api /: botToken /: "getUpdates")
+                        NoReqBody
+                        lbsResponse $
+                        "timeout" =: (30000 :: Int) <>
+                        "offset"  =: offset <>
+                        responseTimeout 60000000
 
-data MessageToSend =  MessageToSend
-  { chat_id  :: Int
-  , text :: T.Text
-  } deriving (Show, Generic)
-
-instance ToJSON MessageToSend
-instance FromJSON MessageToSend
-
-getUpdates :: Req LbsResponse
-getUpdates = req POST
-             (https api /: botToken /: "getUpdates")
-             NoReqBody
-             lbsResponse
-             mempty
 
 sendMessage :: Update -> MessageToSend -> Req LbsResponse
 sendMessage update message = req POST
-             (https api /: botToken /: "sendMessage")
-             (ReqBodyJson message)
-             lbsResponse
-             mempty
+                                 (https api /: botToken /: "sendMessage")
+                                 (ReqBodyJson message)
+                                 lbsResponse
+                                 mempty
 
-defUpdate :: Update
-defUpdate = Update (-1)  Nothing Nothing Nothing Nothing
-
-
-runTelegramBot :: Update -> IO ()
-runTelegramBot oldUpdate = runReq defaultHttpConfig $ do
-  liftIO $ timeout 1000000 (threadDelay 1000 *> pure "tick")
-  response <- getUpdates
+run ::  Integer -> Req LbsResponse
+run  offset = do
+  response <- makeUpdateRequest offset
+  let status = responseStatusCode response
   let body = responseBody response
-  let (Right update) = eitherDecode body :: Either String Updates
-  let lastUpdate = getLastUpdate update
-  if isNewUpdate lastUpdate oldUpdate  
-    then do
-      let message = MessageToSend
-            { chat_id = getChatId lastUpdate
-            , text = fromJust $ getText lastUpdate }
-      sendMessage lastUpdate message
-      liftIO $ print "send"  
-    else liftIO $ print "no message"
-  liftIO $ runTelegramBot lastUpdate
-      
-  
+  let lastUpdate = getLastUpdate =<< decode body
+  liftIO $ print offset
+  case lastUpdate of
+   Nothing -> run offset
+   _ -> do
+     let message = MessageToSend { chat_id = fromJust $ getChatId <$> lastUpdate
+                                , text    = fromJust $ getText <$> fromJust $ lastUpdate }
+     sendMessage (fromJust lastUpdate) message
+     run ( offset +1)
+
 
 main :: IO ()
-main = 
-  runReq defaultHttpConfig $ do
-  response <- getUpdates
+main = do   
+  response <- runReq defaultHttpConfig $ makeUpdateRequest 0
   let body = responseBody response
-  let (Right update) = eitherDecode body :: Either String Updates
-  let oldUpdate = getLastUpdate update -- get  old update
-  liftIO $ runTelegramBot oldUpdate 
-  -- return ()
-
-
+  let oldUpdate = getLastUpdate =<< decode body
+  let offset = getOffset oldUpdate 
+  runReq defaultHttpConfig $ run offset
+  return ()
 
